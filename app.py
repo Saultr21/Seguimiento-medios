585ab132ed9571b56164844f502ca50d6606adb1
Iván <ivandominguezsu@gmail.com>
2026-04-22 11:31:41 +0100

    Cambiado el nombre de uno de los campos del form para ajustarse mejor al resto del programa

--- app.py
+++ app.py
@@ -24,20 +24,19 @@ async def read_root(request: Request):
     # Renderizar la plantilla HTML
     return templates.TemplateResponse(request, "index.html", {"request": request})
 
-
 @app.post("/ejecutar")
 async def ejecutar_stream(
     channel_url: str = Form(...),
     channel_keyword: str = Form(...),
     video_limit: int = Form(...),
     # hacer opcional la lista de menciones para soportar 'Solo transcribir'
-    mention_keyword: List[str] = Form([]),
+    mention_keywords: List[str] = Form([]),
     podcast_limit: int = Form(...),
     single_video_urls: List[str] = Form([]),
     only_transcribe: int = Form(0),
     whisper_language: str = Form("")
 ):
-    mention_keywords_str = ",".join(mention_keyword)
+    mention_keywords_str = ",".join(mention_keywords)
     single_video_urls_str = ",".join(filter(None, single_video_urls)) 
 
     # Usar valores proporcionados por el formulario
@@ -61,6 +60,7 @@ async def ejecutar_stream(
         str(int(bool(only_transcribe))),
         whisper_language
     ]
+
     process = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -70,12 +70,14 @@ async def ejecutar_stream(
         errors='replace',  
         bufsize=1  
     )
+    
     async def log_generator():
         for line in process.stdout:
             print(line, end="", flush=True)  
             yield line                        
         rc = process.wait()
         yield f"\nProceso terminado con código: {rc}\n"
+
     return StreamingResponse(log_generator(), media_type="text/plain")
 
 @app.get("/descargar-csv", response_class=FileResponse)
@@ -84,7 +86,3 @@ async def descargar_csv():
     # Asegúrate de que el nombre del archivo para la descarga sea el deseado.
     # Podrías extraer el nombre del archivo de csv_path si es necesario.
     return FileResponse(path=csv_path, media_type='text/csv', filename="analisis_sentimientos.csv")
-
-
-
-
