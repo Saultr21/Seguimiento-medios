6a273d938d18e1bfcfba573ab3dd919bd1813ba6
Iván Domínguez <ivandominguezsu@gmail.com>
2026-05-08 10:48:38 +0100

    Movidos los importes de la factoría de ASR para evitar la carga inicial de los modelos (lazy loading).

--- src/asr/asr_factory.py
+++ src/asr/asr_factory.py
@@ -1,12 +1,10 @@
-from .whisper_asr import WhisperASR
-from .nemo_asr import NemoASR
-
 class ASRFactory:
-    whisper_asr: None | WhisperASR = None
-    nemo_asr: None | NemoASR = None
+    whisper_asr = None
+    nemo_asr = None
     
     @staticmethod
     def load_whisper_asr(config):
+        from .whisper_asr import WhisperASR
         if ASRFactory.whisper_asr: return ASRFactory.whisper_asr
         
         ASRFactory.whisper_asr = WhisperASR(config)
@@ -14,6 +12,7 @@ class ASRFactory:
 
     @staticmethod
     def load_nemo_asr(config):
+        from .nemo_asr import NemoASR
         if ASRFactory.nemo_asr: return ASRFactory.nemo_asr
         
         ASRFactory.nemo_asr = NemoASR(config)
