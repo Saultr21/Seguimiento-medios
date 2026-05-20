90c578358859c12044c200919ce759dc2dfdae5a
Iván <ivandominguezsu@gmail.com>
2026-05-20 10:04:26 +0100

    Añadida documentación para el backend.

--- src/api/app.py
+++ src/api/app.py
@@ -27,8 +27,13 @@ streamlit_process = None # Variable global para el proceso de Streamlit.
 
 def launch_streamlit():
     """
-    Ejecuta el frontend de Streamlit en un subproceso, permitiendo su acceso y reinicio junto con este backend.
-    Usa el puerto definido en la variable de entorno `VISUALIZATION_PORT`.
+    Inicializa la interfaz de Streamlit como un subproceso.
+
+    Este proceso permite compaginar el frontend de visualización con el backend,
+    compartiendo su ciclo de vida y permitiendo arrancarlo y finalizarlo con facilidad.
+    El proceso se abre con `subprocess.Popen` y se guarda en la variable global `streamlit_process`.
+
+    El puerto donde se sirve se obtiene desde la variable de entorno `VISUALIZATION_PORT`.
     """
 
     global streamlit_process
@@ -44,15 +49,21 @@ def launch_streamlit():
         ],
         stdout = None
     )
+
 # ------------------------------
 # Lifespan de FastAPI
 # ------------------------------
 @contextlib.asynccontextmanager
-async def lifespan(app: FastAPI): # 
+async def lifespan(app: FastAPI):
     """
-    "lifespan" es una variable de FastAPI para añadir lógica de inicio y finalización a la aplicación.
+    Inicializa recursos y los cierra al final del ciclo de vida de FastAPI.
 
-    Utiliza un context manager asíncrono para FastAPI que le permite eejecutar código al iniciar y cerrar la app, separando ambas mitades con un `yield`.
+    Actualmente, carga Streamlit como un subproceso al inicio
+    y lo finaliza al terminar el ciclo de vida.
+
+    Args:
+        app:
+            Instancia principal de FastAPI.
     """
 
     # Lógica de inicio.
@@ -60,7 +71,7 @@ async def lifespan(app: FastAPI): #
     yield # Tras el yield, se añade lógica de apagado.
 
     # Lógica de apagado.
-    if not streamlit_process.returncode:
+    if streamlit_process and not streamlit_process.returncode:
         try:
             streamlit_process.termiate() # Trata de mandar SIGTERM para acabar con el proceso.
         except Exception:
@@ -70,7 +81,7 @@ async def lifespan(app: FastAPI): #
 # ------------------------------
 # Creación de FastAPI
 # ------------------------------
-app = FastAPI(lifespan=lifespan)
+app = FastAPI(lifespan=lifespan) # Le damos el lifespan para que lo ejecute al inicio y al final.
 
 # Monta carpeta "static" para los estilos.
 app.mount("/static", StaticFiles(directory="static"), name="static")
@@ -81,10 +92,22 @@ gr.mount_gradio_app(app, demo.queue(), "/gradio", css_paths=["static/style.css"]
 @app.get("/", response_class=RedirectResponse)
 async def launch_gradio(request: Request):
     """
-    Redirige la raíz '/' hacia el endpoint de Gradio, mostrando la web.
+    Redirige la ruta raíz hacia la interfaz web de Gradio.
 
-    Nota:
-        Colocar Gradio en la raíz daba problemas, así que tuvimos que hacer algo así parece ser necesario.
+    Este endpoint actúa como punto de entrada principal para la aplicación
+    y redirige hacia el endpoint de Gradio, `/gradio`.
+
+    Args:
+        request:
+            Request HTTP entrante.
+
+    Returns:
+        RedirectResponse:
+            Redirección HTTP 301 (permanente) hacia `/gradio`.
+
+    Notes:
+        Montar Gradio en la raíz de FastAPI hacía que el servicio devolviera errores,
+        así que por ello utilizamos una redirección explícita.
     """
 
     # Si tratamos de reemplazar la raíz con Gradio, da errores. Por tanto, redirigimos a su endpoint.
@@ -93,23 +116,85 @@ async def launch_gradio(request: Request):
 @app.get("/visualization-frontend", response_class=RedirectResponse)
 async def redirect_visualization(request: Request):
     """
-    Redirige al frontend de Streamlit, utilizando el mismo puerto `VISUALIZATION_PORT` que para iniciarlo.
+    Redirige hacia el frontend de visualización de datos servido con Streamlit.
+
+    Este endpoint proporciona una redirección rápida hacia el frontend ejecutado
+    como un subproceso con Streamlit.
+
+    Returns:
+        RedirectResponse:
+            Redirección HTTP 301 (permanente) hacia el puerto configurado
+            en la variable de entorno `VISUALIZATION_PORT`.
+
+    Notes:
+        El frontend de Streamlit se ejecuta en un servidor separado,
+        por lo que el acceso se realiza mediante redirección HTTP.
     """
     return RedirectResponse(url=f"localhost:{ os.getenv('VISUALIZATION_PORT') }", status_code=301)
 
 @app.post("/ejecutar")
 async def run_pipeline(
     channel_url: str = Form(...),
-    channel_keyword: Optional[str] = Form(""),
+    channel_keyword: list[str] = Form(""),
     video_limit: int = Form(...),
-    # hacer opcional la lista de menciones para soportar 'Solo transcribir'
-    mention_keywords: List[str] = Form([]),
+    mention_keywords: list[str] = Form([]),
     podcast_limit: int = Form(...),
-    single_video_urls: List[str] = Form([]),
+    single_video_urls: list[str] = Form([]),
     only_transcribe: int = Form(0),
     language: str = Form(""),
     asr_model: str = Form("")
 ):
+    """
+    Ejecuta el pipeline principal de procesamiento multimedia.
+
+    Este endpoint lanza el comando `run-pipeline` (definido en `pipeline:run`) en un
+    subproceso independiente, transmitiendo los logs generados en tiempo real mediante una
+    respuesta HTTP streaming.
+
+    El pipeline permite procesar vídeos de canales, vídeos individuales y tareas de
+    transcripción automática utilizando distintos modelos ASR.
+
+    Args:
+        channel_url:
+            Opcional. URL del canal principal a procesar.
+
+        channel_keyword:
+            Opcional. Palabra clave para buscar en los títulos de vídeos de un canal.
+
+        video_limit:
+            Número máximo de vídeos a procesar desde el canal.
+
+        mention_keywords:
+            Opcional.
+            Lista de entidades (frases) o palabras clave utilizadas para detectar menciones relevantes.
+
+        podcast_limit:
+            Número máximo de podcasts a procesar (de Espejo Canario).
+
+        single_video_urls:
+            Opcional. Lista de URLs individuales de vídeos.
+
+        only_transcribe:
+            Indica si únicamente debe ejecutarse la transcripción.
+
+        language:
+            Idioma utilizado durante el procesamiento ASR.
+
+        asr_model:
+            Modelo ASR utilizado para la transcripción automática.
+            Puede ser `nemo` o `whisper`.
+
+    Returns:
+        StreamingResponse:
+            Flujo de texto en tiempo real con los logs generados durante la ejecución del pipeline.
+
+    Notes:
+        - Lanza un subproceso externo mediante `subprocess.Popen`.
+        - Consume muchos recuross de CPU/GPU (dependiendo del modelo ASR).
+        - Los logs del pipeline se transmiten progresivamente al cliente utilizando streaming
+          HTTP para permitir el seguimiento en tiempo real del procesamiento.
+    """
+
     mention_keywords_str = ",".join(mention_keywords)
     single_video_urls_str = ",".join(filter(None, single_video_urls)) 
     
@@ -158,26 +243,41 @@ async def run_pipeline(
             raise
 
         finally:
-            if process.returncode is None: # Realiza cierre limpio (SIGTERM) si se sigue ejecutando.
+            if not process.returncode: # Realiza cierre limpio (SIGTERM) si se sigue ejecutando.
                 process.terminate()
 
-    return StreamingResponse(log_generator(), media_type="text/plain-text")
+    return StreamingResponse(log_generator(), media_type="text/plain")
 
 @app.get("/descargar-csv", response_class=FileResponse)
 async def download_csv():
     """
-    Devuelve el archivo CSV generado por el pipeline.
+    Devuelve el archivo CSV generado por el pipeline de análisis.
+
+    El endpoint permite descargar el resultado consolidado del procesamiento en formato CSV.
+
+    Returns:
+        FileResponse:
+            Archivo CSV generado por el pipeline.
     """
 
     csv_path = config["csv_output_path"]
-    # Asegúrate de que el nombre del archivo para la descarga sea el deseado.
-    # Podrías extraer el nombre del archivo de csv_path si es necesario.
     return FileResponse(path=csv_path, media_type='text/csv', filename="analisis_sentimientos.csv")
 
 def run():
     """
-    Permite ejecutar el servidor FastAPI con uvicorn y puerto configurable por línea de comando.
-    Se utiliza con el script personalizado `start-server`.
+    Inicia el servidor FastAPI utilizando Uvicorn.
+
+    Este método actúa como punto de entrada del backend y permite configurar el puerto
+    del servidor mediante argumentos de línea de comandos.
+
+    Se utiliza internamente por el script personalizado del proyeccto `start-server`.
+
+    Command Line Arguments:
+        --port:
+            Puerto HTTP utilizado por el servidor FastAPI.
+
+    Notes:
+        - Bloquea el hilo principal hasta detener el servidor.
     """
 
     parser = argparse.ArgumentParser()
